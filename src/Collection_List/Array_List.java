ee2671d6f03f9332fc5ca7c87e93a87ad637ac61
Kalp Soneji <kalpsonejicod@gmail.com>
2025-08-12 12:06:36 +0530

    Collection framework started, ArrayList - 1

---
+++ src/Collection_List/Array_List.java
@@ -0,0 +1,48 @@
+package Collection_List;
+
+import java.util.*;
+
+public class Array_List {
+
+    public static void main(String[] args) {
+
+        ArrayList list = new ArrayList();
+
+        list.add(1);
+        list.add(2.34);
+        list.add("Kalp");
+        list.add(8572L);
+        list.add(5462f);
+        list.add('M');
+
+        Student s1 = new Student(1, "Kalp", 12);
+
+        list.add(s1);
+
+        System.out.println("List size = " + list.size());
+        
+        System.out.println("\nList Elements: ");
+        
+        for (int i = 0; i < list.size(); i++) {
+        
+        	Object obj = list.get(i); 
+            
+        	if (obj instanceof Student) 
+                ((Student) obj).dispData();
+            else 
+                System.out.println(obj); 
+            
+        }
+
+        System.out.println("\nList Elements: ");
+        
+        for (Object obj : list) {
+            
+        	if (obj instanceof Student) 
+                ((Student) obj).dispData(); 
+            else 
+                System.out.println(obj); 
+            
+        }
+    }
+}
---
+++ src/Collection_List/Student.java
@@ -0,0 +1,20 @@
+package Collection_List;
+
+public class Student {
+
+    int rno;
+    String name; 
+    int std;
+
+    public Student() {}
+
+    public Student(int rno, String name, int std) {
+        this.rno = rno;
+        this.name = name;
+        this.std = std;
+    }
+
+    public void dispData() {
+        System.out.println(rno + "\t" + name + "\t" + std);
+    }
+}
